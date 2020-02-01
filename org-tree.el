388e261d8151411eb7678a7d61c44a9cb8e3e8c9
Nathanael Gentry <ngentry1@liberty.edu>
2020-02-01 17:25:29 -0500

    recursion galore

--- org-tree.el
+++ org-tree.el
@@ -32,8 +32,15 @@
 (defconst org-tree-path-separator "/"
   "The separator string with which to delineate path components.")
 
-(defconst org-tree-format-spec '(?i id)
-  "The special formatting options available to org-tree subtree file names.")
+(defun org-tree-testfn (o1 o2)
+  (equal (car o1) o2))
+
+(defconst org-tree-format-spec '(?i id ?h headline)
+  "An alist of the special formatting options available to
+  org-tree subtree file names. Using
+  `org-tree-dynamic-format-spec', each of the symbols referenced
+  here will be dynamically substituted for the dynamically-scoped
+  named symbol.")
 
 (defcustom org-tree-default-subtree-file-name "%i.org"
   "A format string that specifies the default fulename for
@@ -45,15 +52,30 @@ subtree files, including the org extension.")
     :group 'org-tree)
 
 (defvar org-tree-lookup-table nil
-  "A plist containing alists which map file names to logical
-  outline paths (`:physical') and logical outline paths to
-  IDs (`:logical').")
+  "An alist mapping subtree file names to logical paths")
+
+(defun org-tree-format-spec ()
+  (apply #'format-spec-make (org-tree-format-spec-1 org-tree-format-spec)))
+
+(defun org-tree-format-spec-1 (spec &optional new-spec)
+  "Replace each symbol name in SPEC with its value in the current dynamic scope, or nil otherwise."
+  (if spec
+      (progn
+        (setq new-spec
+              (append new-spec (list (car spec) (ignore-errors (symbol-value (cadr spec))))))
+        (org-tree-format-spec-1 (cddr spec) new-spec))
+    new-spec))
+
+(defun true-listp (object)
+  "Return non-`nil' if OBJECT is a true list."
+  (and (listp object)  (null (cdr (last object)))))
 
 (defun org-tree-flatten (list)
   "Nondestructively return a flat version of LIST."
   (cond
    ((null list) nil)
    ((atom list) (list list))
+   ((not (true-listp list)) (list list))
    (t (append (org-tree-flatten (car list)) (org-tree-flatten (cdr list))))))
 
 (defun org-tree-path-list (path)
@@ -81,7 +103,12 @@ in `org-agenda-new-buffers' which might be released later."
     (org-get-agenda-file-buffer file)))
 
 (defun org-tree-resolve-subtree-file-name (&optional pom)
-  "Provide the full file name for the org-tree subtree at POM, or return nil if a subtree does not exist there.
+  "Provide the full file name for the org-tree subtree at POM, or
+  return nil if a subtree does not exist there. This function
+  does not verify that the subtree is actually an org document;
+  it can be any sort of document you wish, which allows easy
+  overloading for other terminal points of your org project
+  tree (say, a LaTeX document).
 
 An org-tree subtree is defined by the presence of an ID from
 `org-id-get', an attachment directory from `org-attach-dir', and
@@ -89,63 +116,83 @@ an extant file in the attachment directory with name specified by
 the SUBTREE property or, lacking that, by the format string
 `org-tree-default-subtree-file-name'."
   (org-with-point-at (or pom (point-marker))
-  (let ((id (org-id-get))
-        (ad (org-attach-dir)))
-    (when (and id ad)
-      (ignore-errors (file-truename (expand-file-name
-       (or (org-entry-get nil "SUBTREE" nil)
-           (format-spec org-tree-default-subtree-file-name (format-spec-make ?i id))) ad)))))))
+    (let ((subtree (org-entry-get nil "SUBTREE" nil))
+          (id (org-id-get))
+          (ad (org-attach-dir)))
+      (if (equal subtree "") subtree
+        (when (and id ad) (ignore-errors (file-truename
+             (expand-file-name
+              (or (org-entry-get nil "SUBTREE" nil)
+                  (format-spec org-tree-default-subtree-file-name (org-tree-format-spec))) ad))))))))
 
-(defun org-tree-lookup-table-1 (path subtree type)
-  "Recursively build the internal lookup plist of type TYPE with initial path PATH
-for the subtree file SUBTREE.
-
-When TYPE is :logical, link outline path strings to IDs for this
-subtree.
-
-When TYPE is :physical, link the physical file of this subtree to
-its outline path entry point."
+(defun org-tree-lookup-table-1 (path subtree &optional ip)
+  "Recursively build the internal lookup alist with initial path PATH
+for the subtree file SUBTREE."
   (with-current-buffer (org-tree-get-file-buffer subtree (org-entry-get nil "NO_AGENDA"))
-    (org-tree-flatten (remove 'nil (org-map-entries
+    (remove 'nil (org-map-entries
       (lambda ()
-        (let* ((subtree (org-tree-resolve-subtree-file-name)))
+        (let ((subtree (org-tree-resolve-subtree-file-name)))
           (when subtree
-            (let* ((path (if (or path (string= (file-truename (buffer-file-name)) (file-truename org-tree-root)))
+            (let* ((path (if (or path (equal (file-truename (buffer-file-name)) (file-truename org-tree-root)))
                          (append path (unless (org-before-first-heading-p)
                                         (ad-with-originals 'org-get-outline-path
                                           (org-get-outline-path t))))
                        (org-tree-outline-path nil t)))
                    (spath (org-tree-path-string path))
-                   (subtree-exists (ignore-errors (file-exists-p subtree)))
-               (app (cond ((eq type :physical) (when subtree-exists (list subtree spath)))
-                          ((eq type :logical) (list spath (org-id-get)))
-                          (t (user-error "Bad lookup plist type")))))
-              (if subtree-exists (append app (org-tree-lookup-table-1 path subtree type))
+                   (subtree-exists (and (not (equal subtree ""))
+                                        (equal (file-name-extension subtree) "org")
+                                        (ignore-errors (file-exists-p subtree))))
+                   (app (when subtree-exists (cons (list subtree (org-id-get)) spath)))
+                   (rec (when subtree-exists (org-tree-lookup-table-1 path subtree ip))))
+              (if (and subtree-exists rec)
+                  (append (list app) rec)
                 app)))))
-      t 'file)))))
+      t 'file))))
 
-(defun org-tree-lookup-table (&optional type force)
-  "Return the entry path table of TYPE. If FORCE,
-recalculate both tables anew before returning the requested table."
+(defun org-tree-lookup-table (&optional force)
+  "Return the entry path table, calculating it if necessary. If FORCE,
+recalculate both tables anew before returning the requested
+table.
+
+The lookup table is stored as an alist, where each association
+has `car' as a list whose `car' is the physical path to the
+subtree file and chose `cdr' is the subtree's ID, and has as
+`cdr' the logical outline path of the subtree."
   (unless org-tree-root (user-error "Tree index cannot be nil"))
   (unless (and org-tree-lookup-table (not force))
-    (let (org-agenda-new-buffers
-          org-mode-hook)
-      (setq org-tree-lookup-table
-            (list :physical (org-tree-lookup-table-1 nil org-tree-root :physical)
-                  :logical (org-tree-lookup-table-1 nil org-tree-root :logical)))
+    (let (org-agenda-new-buffers org-mode-hook)
+      (setq org-tree-lookup-table (org-tree-flatten (org-tree-lookup-table-1 nil org-tree-root)))
       (setq org-agenda-files org-id-files)
       (org-release-buffers org-agenda-new-buffers)))
-  (if type (plist-get org-tree-lookup-table type)
-    org-tree-lookup-table))
+      org-tree-lookup-table)
 
-(defun org-tree-lookup-1 (path type)
-  "Get the value corresponding to PATH in lookup table TYPE. Note
-that the root path has an ID of nil, and its path is `org-tree-root'."
+(defun org-tree-lookup (path)
+  "Get the outline path corresponding to the subtree at path
+PATH. This function does not interpolate paths from roots;
+it directly maks from `org-tree-lookup-table'. Note that the root
+path has an ID of nil, and its path is `org-tree-root'."
   (let ((path (org-tree-path-string path)))
-    (if (string= path "") ""
-      (if (string= path org-tree-root) "/"
-    (lax-plist-get (org-tree-lookup-table type) path)))))
+    (cond ((equal path org-tree-root) "/")
+           (t (cdr (assoc path (org-tree-lookup-table) (lambda (o1 o2) (equal (car o1) o2))))))))
+
+(defun org-tree-reverse-lookup (path &optional lax)
+  "With logical subtree path PATH, return the entry of
+`org-tree-lookup-table' that defines the exact subtree. With LAX,
+return the highest subtree for which a match is found."
+  (let ((rassoc (cl-rassoc (org-tree-path-string path) (org-tree-lookup-table)
+                           :test (lambda (a b) (funcall (if lax #'string-prefix-p #'equal) b a)))))
+    (cond  (rassoc (list (car rassoc)
+                         (append (list (cdr rassoc))
+                                 (or (ignore-errors (substring (string-remove-prefix (cdr rassoc) path) 1)) ""))))
+           (t (list (list org-tree-root nil) (cons org-tree-path-separator (substring path 1)))))))
+
+(defun org-tree-find-olp (path)
+  "Return a marker to the place where PATH is defined."
+  (let* ((info (org-tree-reverse-lookup (org-tree-path-string path) :lax))
+        (path (org-tree-path-list (cdadr info))))
+    (cond ((and (equal (cdadr info) "") path) (org-id-find (cadar info) t))
+          (path (org-find-olp (append (list (caar info)) path)))
+          (t (set-marker (make-marker) 0 (org-get-agenda-file-buffer (caar info)))))))
 
 (defun org-tree-outline-path (&optional func with-self use-cache as-string)
   "Return the logical path of the physical headline at point.
@@ -155,9 +202,9 @@ framework.
 When the optional argument WITH-SELF is non-nil, the path aldo
 includes the current headline. See `org-get-outline-path' for
 documentation of USE-CACHE."
-  (ad-with-originals 'org-get-outline-path
+   (ad-with-originals 'org-get-outline-path
     (let ((res (append
-                (org-tree-path-list (or (lax-plist-get (org-tree-lookup-table :physical) (file-truename (buffer-file-name))) ""))
+                (org-tree-path-list (or (org-tree-lookup (file-truename (buffer-file-name))) ""))
                 (org-get-outline-path with-self use-cache))))
       (if as-string (org-tree-path-string res) res))))
 
@@ -172,4 +219,4 @@ element in the perspective tree."
         (advice-add 'org-get-outline-path :around #'org-tree-outline-path))
     (advice-remove 'org-get-outline-path #'org-tree-outline-path)))
 
-(provide 'org-tree-mode)
+(provide 'org-tree)
